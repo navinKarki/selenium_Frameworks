b45d60bac2a475edb94791ef236c1338ce9098ca
navin <navin@DESKTOP-E76I3N8.lan>
2022-05-10 21:41:23 -0500

    initial commit

---
+++ src/test/java/script/Demo4.java
@@ -0,0 +1,28 @@
+package script;
+
+import java.lang.reflect.Method;
+
+import org.testng.Reporter;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class Demo4 {
+
+	@BeforeMethod
+    public void pc(Method m) {
+		String name=m.getName();
+		Reporter.log("Next method is:"+name,true);
+	}
+	
+	@Test
+	public void testA()
+	{
+		Reporter.log("testA",true);
+	}
+	
+	@Test
+	public void testB()
+	{
+		Reporter.log("testB",true);
+	}
+}
